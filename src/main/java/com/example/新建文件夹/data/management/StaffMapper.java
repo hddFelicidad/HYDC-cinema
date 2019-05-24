090f692483cf7dfa3364caa8cb8cea558ff7d421
weirdoheheelf <171250036@smail.nju.edu.cn>
2019-05-24 14:00:00 +0100

    影院管理数据层

---
+++ src/main/java/com/example/新建文件夹/data/management/StaffMapper.java
@@ -0,0 +1,45 @@
+package com.example.cinema.data.management;
+
+import com.example.cinema.po.Staff;
+import com.example.cinema.vo.StaffForm;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author poh
+ * @date 2019/5/11 5:00 PM
+ */
+@Mapper
+public interface StaffMapper {
+    /**
+     * 插入一个影院员工
+     * @param staffForm
+     * @return
+     */
+    int insertOneStaff(StaffForm staffForm);
+    /**
+     * 查询所有影院员工
+     * @return
+     */
+    List<Staff> selectAllStaff();
+    /**
+     * 根据员工id查询影院员工
+     * @param staffId
+     * @return
+     */
+    Staff selectStaffById(@Param("staffId") int staffId);
+    /**
+     * 更新影院员工信息
+     * @param staffForm
+     * @return
+     */
+    int updateStaff(StaffForm staffForm);
+    /**
+     * 根据员工id删除影院员工
+     * @param staffId
+     * @return
+     */
+    int deleteStaff(int staffId);
+}
