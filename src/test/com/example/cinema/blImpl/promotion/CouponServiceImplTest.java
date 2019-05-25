4e4a02c0f4bd49df264e652965e4595eb9674d77
hddFelicidad <1635607919@qq.com>
2019-05-25 23:00:45 +0100

    单元测试修正

---
+++ src/test/com/example/cinema/blImpl/promotion/CouponServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.example.cinema.blImpl.promotion;
+
+import com.example.cinema.po.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class CouponServiceImplTest {
+    @Autowired
+    CouponServiceImpl couponService;
+
+    @Test
+    public void getCouponsByUser() {
+
+    }
+
+    @Test
+    public void addCoupon() {
+
+    }
+
+    @Test
+    public void issueCoupon() {
+    }
+
+    @Test
+    public void getCouponById() {
+    }
+
+    @Test
+    public void getCouponListByUserId() {
+    }
+
+    @Test
+    public void deleteCouponUser() {
+    }
+}
