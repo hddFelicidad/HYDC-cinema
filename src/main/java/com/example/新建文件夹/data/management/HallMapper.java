3a3f957091348b3752ef8fdf421c97c2bb688c91
1215948922@qq.com <171250588>
2019-05-25 14:00:00 +0100

    管理员后端-影厅管理v1.0

---
+++ src/main/java/com/example/新建文件夹/data/management/HallMapper.java
@@ -0,0 +1,49 @@
+package com.example.cinema.data.management;
+
+import com.example.cinema.po.Hall;
+import com.example.cinema.vo.HallForm;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author fjj
+ * @date 2019/4/11 3:46 PM
+ */
+@Mapper
+public interface HallMapper {
+    /**
+     * 查询所有影厅信息
+     * @return
+     */
+    List<Hall> selectAllHall();
+
+    /**
+     * 根据id查询影厅
+     * @return
+     */
+    Hall selectHallById(@Param("hallId") int hallId);
+
+    /**
+     * 插入一条电影信息
+     * @param addHallForm
+     * @return
+     */
+    int insertOneHall(HallForm addHallForm);
+
+    /**
+     * 修改电影
+     * @param updateHallForm
+     * @return
+     */
+    int updateHall(HallForm updateHallForm);
+
+    /**
+     * 删除影厅
+     * @param hallId
+     * @return
+     */
+    int deleteHall(int hallId);
+
+}
